87f0065d26b2b1ace0c9abde74639173e3546de3
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-01-19 15:09:34 +0000

    Add WSGI config for PolicyLens

---
+++ config/wsgi.py
@@ -0,0 +1,10 @@
+# path: policylens/config/wsgi.py
+"""WSGI config for PolicyLens."""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+application = get_wsgi_application()
