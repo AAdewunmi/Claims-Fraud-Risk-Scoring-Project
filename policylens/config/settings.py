af110d4b0a3edf36fb65ce7cc906c48f549cd7b6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-02-16 16:14:55 +0000

    chore(settings): add staticfiles wiring for project assets

--- policylens/config/settings.py
+++ policylens/config/settings.py
@@ -91,7 +91,11 @@ TIME_ZONE = "Europe/London"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR.parent / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR.parent / "static",
+]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
