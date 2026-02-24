f2bbf679dbb427d1de70474e488f21576b2e031d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-02-24 11:02:31 +0000

    feat(public): add Django AppConfig for public app

---
+++ policylens/apps/public/apps.py
@@ -0,0 +1,15 @@
+"""
+Public site surfaces for PolicyLens.
+
+This app owns the unauthenticated landing page and other public-facing pages
+that should not route through Django admin login plumbing.
+"""
+
+from django.apps import AppConfig
+
+
+class PublicConfig(AppConfig):
+    """Django AppConfig for the public site surfaces."""
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "policylens.apps.public"
+    verbose_name = "Public"
