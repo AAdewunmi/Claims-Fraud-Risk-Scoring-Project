0559c29663be290d04bd359b8caeb12f56fc6570
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-03-04 16:57:01 +0000

    chore: refactor Gunicorn worker configuration for improved readability

--- docker/gunicorn/gunicorn.conf.py
+++ docker/gunicorn/gunicorn.conf.py
@@ -12,9 +12,7 @@ import multiprocessing
 import os
 
 bind = os.getenv("GUNICORN_BIND", "0.0.0.0:8000")
-workers = int(
-    os.getenv("GUNICORN_WORKERS", str(max(2, multiprocessing.cpu_count() * 2 + 1)))
-)
+workers = int(os.getenv("GUNICORN_WORKERS", str(max(2, multiprocessing.cpu_count() * 2 + 1))))
 worker_class = os.getenv("GUNICORN_WORKER_CLASS", "sync")
 threads = int(os.getenv("GUNICORN_THREADS", "1"))
 timeout = int(os.getenv("GUNICORN_TIMEOUT", "60"))
